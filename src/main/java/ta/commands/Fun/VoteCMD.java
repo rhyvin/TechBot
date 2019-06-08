211858fdc70ed1f672185d51f3af4ad3c6b6cc1e
Rhyvin <roman@technoanomaly.com>
2019-06-07 20:14:01 -0400

    prep for Vote command

---
+++ src/main/java/ta/commands/Fun/VoteCMD.java
@@ -0,0 +1,26 @@
+package ta.commands.Fun;
+
+
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import ta.util.IntCommand;
+
+import java.io.IOException;
+import java.util.List;
+
+public class VoteCMD implements IntCommand {
+
+    @Override
+    public void handle(List<String> args, GuildMessageReceivedEvent event) throws ClassNotFoundException, IOException {
+
+    }
+
+    @Override
+    public String getHelp() {
+        return null;
+    }
+
+    @Override
+    public String getInvoke() {
+        return null;
+    }
+}
