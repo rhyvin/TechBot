bf0722f7e22e2a3f5b3c114733f7983752aa8c3b
Rhyvin <roman@technoanomaly.com>
2019-08-15 09:43:53 -0400

    Updated .gitignore
    Ignoring files that are not required for publishing

--- src/main/java/ta/commands/Fun/JokeCMD.java
+++ src/main/java/ta/commands/Fun/JokeCMD.java
@@ -1,5 +1,6 @@
 package ta.commands.Fun;
-/*
+
+import com.fasterxml.jackson.databind.JsonNode;
 import me.duncte123.botcommons.messaging.EmbedUtils;
 import me.duncte123.botcommons.web.WebUtils;
 import net.dv8tion.jda.core.entities.MessageEmbed;
@@ -7,39 +8,26 @@ import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import ta.util.IntCommand;
 
 import java.util.List;
-import java.util.Random;
 
 public class JokeCMD implements IntCommand {
 
-    private final Random random;
-
-    public JokeCMD(Random random) {
-        this.random = random;
-    }
-
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
-        WebUtils.ins.getJSONObject("https://sv443.net/jokeapi/category/Any?blacklistFlags=nsfw,dark,religious,political").async( (json) -> {
-
-            if(json.getString("type").equalsIgnoreCase("single")){
-                String text = json.getString("joke");
-                MessageEmbed embed = EmbedUtils.embedMessage(text).build();
-                //TODO: Make a permission check to see if the bot can send embeds if not, send plain text.
-                event.getChannel().sendMessage(embed).queue();
-            }
-            String setup = json.getString("setup");
-            String delivery = json.getString("delivery");
-            MessageEmbed embed = EmbedUtils.embedMessage(setup + "\n\n||" + delivery + "||" ).build();
-            //TODO: Make a permission check to see if the bot can send embeds if not, send plain text.
+        WebUtils.ins.getJSONObject("https://apis.duncte123.me/joke?nsfw=false").async( (json) -> {
+            JsonNode data = json.get("data");
+            String url = data.get("image").asText();
+            MessageEmbed embed = EmbedUtils.embedImage(url)
+                    .setTitle(data.get("title").asText(), data.get("url").asText())
+                    .build();
+            //TODO: Make a permissions check to see if the bot can send embeds if not, send plain text
             event.getChannel().sendMessage(embed).queue();
-            MessageEmbed embed1 = EmbedUtils.embedMessage(delivery).build();
         });
 
     }
 
     @Override
     public String getHelp() {
-        return "Shows you a random meme.";
+        return "Shows you a random joke.";
     }
 
     @Override
@@ -47,5 +35,3 @@ public class JokeCMD implements IntCommand {
         return "joke";
     }
 }
-
- */
